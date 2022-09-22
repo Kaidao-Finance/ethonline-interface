ce186173f5042124fddc5336b0f753f81a481e0f
Tan <pochara.youcharoen@gmail.com>
2022-09-22 18:11:51 +0700

    fix: ws server

--- src/contexts/SocketContext.tsx
+++ src/contexts/SocketContext.tsx
@@ -1,7 +1,7 @@
 import React, { createContext } from "react";
 import { io, Socket } from "socket.io-client";
 
-const socket = io("http://localhost:3001", {
+const socket = io("https://ethernal-ws.kmuttchain.tech", {
   transports: ["websocket"],
 });
 export const SocketContext = createContext<{ socket: Socket }>({ socket });
